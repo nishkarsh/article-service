49228ecd405ad27eef91addea463ec7d1d7aad1a
Nishkarsh Sharma <nishkarsh.sharma@sezzle.com>
2022-08-19 20:13:58 +0200

    Add integration tests for `DELETE /articles/{id}` endpoint.

--- src/test/kotlin/io/github/nishkarsh/publishing/articleservice/controllers/ArticleControllerIntegrationTest.kt
+++ src/test/kotlin/io/github/nishkarsh/publishing/articleservice/controllers/ArticleControllerIntegrationTest.kt
@@ -9,6 +9,8 @@ import org.bson.types.ObjectId
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.hasSize
 import org.hamcrest.Matchers.`is`
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -16,11 +18,13 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.findById
 import org.springframework.http.HttpHeaders.LOCATION
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
@@ -72,6 +76,25 @@ class ArticleControllerIntegrationTest {
 	@Test
 	internal fun shouldReturn404WhenArticleNotFound(@Random articleId: ObjectId) {
 		mockMvc.perform(MockMvcRequestBuilders.get("/articles/{id}", articleId))
+			.andExpect(status().isNotFound)
+			.andExpect(content().json("{\"message\":\"Could not find article with ID: ${articleId}\"}"))
+	}
+
+	@Test
+	internal fun shouldDeleteArticle(@Random article: Article) {
+		val insertedArticle = mongoTemplate.insert<Article>(article)
+		assertNotNull(insertedArticle)
+
+		mockMvc.perform(delete("/articles/{id}", article.id))
+			.andExpect(status().isNoContent)
+
+		val foundArticle = mongoTemplate.findById<Article>(article.id!!)
+		assertNull(foundArticle)
+	}
+
+	@Test
+	internal fun shouldReturn404WhenArticleNotFoundForDeletion(@Random articleId: ObjectId) {
+		mockMvc.perform(delete("/articles/{id}", articleId))
 			.andExpect(status().isNotFound)
 			.andExpect(content().json("{\"message\":\"Could not find article with ID: ${articleId}\"}"))
 	}
