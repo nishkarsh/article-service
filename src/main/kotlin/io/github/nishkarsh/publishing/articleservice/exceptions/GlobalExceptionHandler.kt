5933a2b86bd1ab3b1d340c6d1b7ebce1dd7e9ed7
Nishkarsh Sharma <nishkarsh.sharma@sezzle.com>
2022-08-20 17:49:26 +0200

    Add exception handler implementations.

--- src/main/kotlin/io/github/nishkarsh/publishing/articleservice/exceptions/GlobalExceptionHandler.kt
+++ src/main/kotlin/io/github/nishkarsh/publishing/articleservice/exceptions/GlobalExceptionHandler.kt
@@ -1,12 +1,15 @@
 package io.github.nishkarsh.publishing.articleservice.exceptions
 
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatus.NOT_FOUND
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
-@ControllerAdvice
+@RestControllerAdvice
 class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
 
 	@ExceptionHandler(value = [ArticleNotFoundException::class])
@@ -14,6 +17,18 @@ class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
 		return ResponseEntity.status(NOT_FOUND)
 			.body(ErrorResponse(exception.message))
 	}
+
+	@ExceptionHandler(value = [Exception::class])
+	protected fun handleInternalServerError(ex: Exception): ResponseEntity<ErrorResponse> {
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+			.body(ErrorResponse(ex.message))
+	}
+
+	override fun handleExceptionInternal(
+		exception: Exception, body: Any?, headers: HttpHeaders, status: HttpStatus, request: WebRequest
+	): ResponseEntity<Any> {
+		return super.handleExceptionInternal(exception, ErrorResponse(exception.message), headers, status, request)
+	}
 }
 
-data class ArticleNotFoundException(override val message: String) : Throwable(message)
+data class ArticleNotFoundException(override val message: String) : RuntimeException(message)
