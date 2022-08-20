18bda55bd7484fc170e41e7a06475029609da1e8
Nishkarsh Sharma <nishkarsh.sharma@sezzle.com>
2022-08-20 18:12:53 +0200

    Add indexes on searchable fields.

--- src/main/kotlin/io/github/nishkarsh/publishing/articleservice/models/Article.kt
+++ src/main/kotlin/io/github/nishkarsh/publishing/articleservice/models/Article.kt
@@ -3,33 +3,49 @@ package io.github.nishkarsh.publishing.articleservice.models
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonProperty.Access.READ_ONLY
 import org.bson.types.ObjectId
+import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.data.mongodb.core.mapping.Field
 import java.time.ZonedDateTime
 
-@Document("articles")
+@Document(Article.COLLECTION)
 data class Article(
-	@JsonProperty("id", access = READ_ONLY)
+	@JsonProperty(ID, access = READ_ONLY)
 	val id: ObjectId?,
 
-	@JsonProperty("header")
+	@Field(HEADER)
+	@JsonProperty(HEADER)
 	val header: String,
 
-	@JsonProperty("shortDescription")
+	@Field(SHORT_DESCRIPTION)
+	@JsonProperty(SHORT_DESCRIPTION)
 	val shortDescription: String,
 
-	@JsonProperty("text")
+	@Field(TEXT)
+	@JsonProperty(TEXT)
 	val text: String,
 
-	@JsonProperty("publishDate")
+	@Indexed
+	@Field(PUBLISH_DATE)
+	@JsonProperty(PUBLISH_DATE)
 	val publishDate: ZonedDateTime,
 
-	@JsonProperty("authors")
+	@Indexed
+	@Field(AUTHORS)
+	@JsonProperty(AUTHORS)
 	val authors: ArrayList<String>,
 
-	@JsonProperty("keywords")
+	@Indexed
+	@Field(KEYWORDS)
+	@JsonProperty(KEYWORDS)
 	val keywords: ArrayList<String>,
 ) {
 	companion object {
+		const val COLLECTION = "articles"
+		const val ID = "id"
+		const val HEADER = "header"
+		const val SHORT_DESCRIPTION = "shortDescription"
+		const val TEXT = "text"
 		const val AUTHORS = "authors"
 		const val PUBLISH_DATE = "publishDate"
 		const val KEYWORDS = "keywords"
